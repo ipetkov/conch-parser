5b61a194776cdc7ee90ba2f7afe7968704e14ffc
Ivan Petkov <ivanppetkov@gmail.com>
2016-03-20 10:13:27 -0700

    Deny print_stdout and wrong_self_convention clippy lints
    * Also loosen the semver restriction on clippy, use ^0.0 instead of
      0.0.x

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 
+#![cfg_attr(feature = "clippy", deny(print_stdout))]
+#![cfg_attr(feature = "clippy", deny(wrong_self_convention))]
+
 #![deny(missing_copy_implementations)]
 #![deny(missing_debug_implementations)]
 #![deny(missing_docs)]
