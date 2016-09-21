f5d62e5c3280c9ee62ad38221928f862cc2025fc
Ivan Petkov <ivanppetkov@gmail.com>
2016-09-20 19:01:41 -0700

    Add an example on setting up a parser and reading input

---
+++ examples/parsing_input.rs
@@ -0,0 +1,39 @@
+#![cfg_attr(feature = "nightly", feature(io))]
+
+extern crate shell_lang as shell;
+
+use std::io::{self, Read};
+use shell::lexer::Lexer;
+use shell::parse::DefaultParser;
+
+#[cfg(feature = "nightly")]
+fn run_with_stdin<F>(f: F) where F: FnOnce(&mut Iterator<Item = char>) {
+    // Read::chars is currently feature gated
+    let mut stdin = io::stdin().chars().map(Result::unwrap);
+    f(&mut stdin);
+}
+
+#[cfg(not(feature = "nightly"))]
+fn run_with_stdin<F>(f: F) where F: FnOnce(&mut Iterator<Item = char>) {
+    // There's no "easy" way to convert stdin into a char iterator on stable
+    // (and by easy I mean without writing our own iterator wrapper) so
+    // we'll just read all input into memory at once to get the point across.
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let mut stdin = input.chars();
+    f(&mut stdin);
+}
+
+
+fn main() {
+    run_with_stdin(|stdin| {
+        // Initialize our token lexer and shell parser with the program's input
+        let lex = Lexer::new(stdin);
+        let parser = DefaultParser::new(lex);
+
+        // Parse our input!
+        for t in parser {
+            println!("{:?}", t);
+        }
+    });
+}
