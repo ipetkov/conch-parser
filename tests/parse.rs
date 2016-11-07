5938c2887eff5e3e350e7a8f48f906ff0a3fb224
Ivan Petkov <ivanppetkov@gmail.com>
2016-11-06 19:16:47 -0800

    Move pipeline tests to their own module

--- tests/parse.rs
+++ tests/parse.rs
@@ -135,55 +135,6 @@ fn test_and_or_invalid_with_newlines_before_operator() {
 }
 
 #[test]
-fn test_pipeline_valid_bang() {
-    let mut p = make_parser("! foo | bar | baz");
-    let correct = CommandList {
-        first: ListableCommand::Pipe(true, vec!(
-            Simple(cmd_simple("foo")),
-            Simple(cmd_simple("bar")),
-            Simple(cmd_simple("baz")),
-        )),
-        rest: vec!(),
-    };
-    assert_eq!(correct, p.and_or_list().unwrap());
-}
-
-#[test]
-fn test_pipeline_valid_bangs_in_and_or() {
-    let mut p = make_parser("! foo | bar || ! baz && ! foobar");
-    let correct = CommandList {
-        first: ListableCommand::Pipe(true, vec!(
-            Simple(cmd_simple("foo")),
-            Simple(cmd_simple("bar"))
-        )),
-        rest: vec!(
-            AndOr::Or(ListableCommand::Pipe(true, vec!(
-                Simple(cmd_simple("baz")),
-            ))),
-            AndOr::And(ListableCommand::Pipe(true, vec!(
-                Simple(cmd_simple("foobar")),
-            ))),
-        ),
-    };
-    assert_eq!(correct, p.and_or_list().unwrap());
-}
-
-#[test]
-fn test_pipeline_no_bang_single_cmd_optimize_wrapper_out() {
-    let mut p = make_parser("foo");
-    let parse = p.pipeline().unwrap();
-    if let ListableCommand::Pipe(..) = parse {
-        panic!("Parser::pipeline should not create a wrapper if no ! present and only a single command");
-    }
-}
-
-#[test]
-fn test_pipeline_invalid_multiple_bangs_in_same_pipeline() {
-    let mut p = make_parser("! foo | bar | ! baz");
-    assert_eq!(Err(Unexpected(Token::Bang, src(14, 1, 15))), p.pipeline());
-}
-
-#[test]
 fn test_comment_cannot_start_mid_whitespace_delimited_word() {
     let mut p = make_parser("hello#world");
     let w = p.word().unwrap().expect("no valid word was discovered");
